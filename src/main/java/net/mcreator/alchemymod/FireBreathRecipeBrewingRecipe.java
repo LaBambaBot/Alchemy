05061d5f973a4750f49d9b7b8543cc61423c284d
LaBambaBot <Ross Kondo@CTE-HPENVY.qsd.wednet.edu>
2021-11-19 10:10:05 -0800

    Added recipe for FireBreath Potion , Started working on fixing Health boose on Steelbody potion

---
+++ src/main/java/net/mcreator/alchemymod/FireBreathRecipeBrewingRecipe.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.alchemymod;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.potion.Potions;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+
+import net.mcreator.alchemymod.potion.FireDamagePotion;
+
+@AlchemyModModElements.ModElement.Tag
+public class FireBreathRecipeBrewingRecipe extends AlchemyModModElements.ModElement {
+	public FireBreathRecipeBrewingRecipe(AlchemyModModElements instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		BrewingRecipeRegistry.addRecipe(new CustomBrewingRecipe());
+	}
+	public static class CustomBrewingRecipe implements IBrewingRecipe {
+		@Override
+		public boolean isInput(ItemStack input) {
+			Item inputItem = input.getItem();
+			return (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION)
+					&& PotionUtils.getPotionFromItem(input) == Potions.AWKWARD;
+		}
+
+		@Override
+		public boolean isIngredient(ItemStack ingredient) {
+			return ingredient.getItem() == Items.LAVA_BUCKET;
+		}
+
+		@Override
+		public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+			if (isInput(input) && isIngredient(ingredient)) {
+				return PotionUtils.addPotionToItemStack(new ItemStack(input.getItem()), FireDamagePotion.potionType);
+			}
+			return ItemStack.EMPTY;
+		}
+	}
+}
