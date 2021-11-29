a8bec028da22570b85c3c3fc611aa1636485261a
LaBambaBot <Ross Kondo@CTE-HPENVY.qsd.wednet.edu>
2021-11-29 11:42:04 -0800

    Started Testing Events . Started Working With doggy( Spawn Wolf)

---
+++ src/main/java/net/mcreator/alchemymod/procedures/DoggyEffectStartedappliedProcedure.java
@@ -0,0 +1,8 @@
+package net.mcreator.alchemymod.procedures;
+
+import java.util.Map;
+
+public class DoggyEffectStartedappliedProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
