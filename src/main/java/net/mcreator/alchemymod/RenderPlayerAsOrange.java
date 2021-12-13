d81ba45b7dcdede61ffb7816d7fa251d6300f39f
LaBambaBot <Ross Kondo@CTE-HPENVY.qsd.wednet.edu>
2021-12-13 11:50:33 -0800

    Import , what needed to continue and run code to bug fix

--- src/main/java/net/mcreator/alchemymod/RenderPlayerAsOrange.java
+++ src/main/java/net/mcreator/alchemymod/RenderPlayerAsOrange.java
@@ -1,4 +1,11 @@
 package net.mcreator.alchemymod;
-
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+@Mod.EventBusSubscriber(modid = "alchemy_mod", bus = Bus.FORGE, value = Dist.CLIENT)
 public class RenderPlayerAsOrange {
+	@SubscribeEvent
+	public static void renderPlayer(final RenderPlayerEvent.Pre event){
+		event.setCanceled(true);
+	} 
 }
