9a71ecf165acd5f412e11dc23d2dc25b7c380cca
LaBambaBot <Ross Kondo@CTE-HPENVY.qsd.wednet.edu>
2021-12-14 11:35:07 -0800

    Got all imports working and Does not load player model.

--- src/main/java/net/mcreator/alchemymod/RenderPlayerAsOrange.java
+++ src/main/java/net/mcreator/alchemymod/RenderPlayerAsOrange.java
@@ -1,11 +1,14 @@
 package net.mcreator.alchemymod;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-@Mod.EventBusSubscriber(modid = "alchemy_mod", bus = Bus.FORGE, value = Dist.CLIENT)
+import net.minecraftforge.api.distmarker.Dist;
+
+@Mod.EventBusSubscriber(modid = "alchemy_mod",value = Dist.CLIENT)
 public class RenderPlayerAsOrange {
 	@SubscribeEvent
-	public static void renderPlayer(final RenderPlayerEvent.Pre event){
+	public static void renderPlayer(final RenderPlayerEvent.Pre event) {
 		event.setCanceled(true);
-	} 
+	}
 }
